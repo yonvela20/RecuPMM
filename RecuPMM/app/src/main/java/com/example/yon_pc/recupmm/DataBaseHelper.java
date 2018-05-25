f480f61d1fcaf4bdbf954c9d3ef8ae711c4dbe24
yonvela20 <yon.vela20@gmail.com>
2018-05-25 13:05:37 +0100

    añadida la base de datos

---
+++ RecuPMM/app/src/main/java/com/example/yon_pc/recupmm/DataBaseHelper.java
@@ -0,0 +1,36 @@
+package com.example.yon_pc.recupmm;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Yon-PC on 25/05/2018.
+ */
+
+public class DataBaseHelper extends SQLiteOpenHelper{
+
+    String usuarios = "CREATE TABLE Usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT,usuario TEXT, password TEXT)";
+    String viajes = "CREATE TABLE Viajes (Origen TEXT, Destino TEXT, Clase TEXT, Precio DOUBLE)";
+    String billetes = "CREATE TABLE Billetes (usuarios TEXT, Origen TEXT, Destino TEXT, Clase TEXT, Precio DOUBLE, Extras TEXT, " +
+            "FOREIGN KEY (usuarios) REFERENCES Usuarios (id))";
+
+
+    public DataBaseHelper(Context contexto, String nombre, SQLiteDatabase.CursorFactory almacen, int version) {
+        super(contexto, nombre, almacen, version);
+    }
+
+    public void onCreate(SQLiteDatabase bd) {
+        bd.execSQL(usuarios);
+        bd.execSQL(viajes);
+        bd.execSQL(billetes);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase bd, int versionAnterior, int versionNueva) {
+        //Eliminamos la version anterior de la tabla
+        bd.execSQL("DROP TABLE IF EXISTS Usuarios");
+        bd.execSQL(usuarios);
+
+    }
+}
